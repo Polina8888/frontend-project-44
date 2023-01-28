6efc0c4f63bf28e64e1d17e037a15a77bf774ebd
mag <polina.arutyunyan.08@mail.ru>
2023-01-29 00:09:42 +0300

    fix even.js for brain-even game

---
+++ src/games/even.js
@@ -0,0 +1,13 @@
+import greeting from '../cli.js';
+import getRandomNumber from '../math.js';
+import { getPlayerAnswer, checkingForCorrectAnswer } from '../index.js';
+
+export default () => {
+  const name = greeting('Answer "yes" if the number is even, otherwise answer "no".');
+  for (let i = 0; i < 3; i += 1) {
+    const num = getRandomNumber();
+    const playerAnswer = getPlayerAnswer(num);
+    const correctAnswer = (num % 2 === 0) ? 'yes' : 'no';
+    if (!checkingForCorrectAnswer(correctAnswer, playerAnswer, i, name)) break;
+  }
+};
